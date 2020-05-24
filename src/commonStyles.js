bdd774125392fca5eaf13a5c3a1c96434e9f2074
zjefersound <jeferson.2017317264@gmail.com>
2020-05-24 19:59:34 -0300

    Criação projeto shoplist

---
+++ src/commonStyles.js
@@ -0,0 +1,13 @@
+export default {
+    fontFamily: {
+        roboto: 'Roboto',
+    },
+    colors: {
+        primary: '#F47E82',
+        primary_dark: '#CC6F77',
+        secondary: '#F6F1C9',
+        secondary_dark: '#F6C9AA',
+        brown: '#846666',
+        green: '#698A68',
+    } 
+};
